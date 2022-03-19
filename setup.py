cedcf163168f6114a9683b0ac6c7587bc04093c0
Pedro Sousa Lacerda <pslacerda@gmail.com>
2022-03-19 11:11:26 -0300

    bump version

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ README = (HERE / "README.md").read_text()
 
 setup(
     name="pymol-labimm",
-    version="0.13.6",
+    version="0.13.7",
     description="Some PyMOL utilities",
     long_description=README,
     long_description_content_type="text/markdown",
